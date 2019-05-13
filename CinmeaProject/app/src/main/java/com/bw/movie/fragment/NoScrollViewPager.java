9ef5c46c7a52b4ad28975d4c6ebc8aef99a979f5
Liberty <1850915912@qq.com>
2019-05-13 08:21:17 +0800

    第三次提交

--- CinmeaProject/app/src/main/java/com/bw/movie/fragment/NoScrollViewPager.java
+++ CinmeaProject/app/src/main/java/com/bw/movie/fragment/NoScrollViewPager.java
@@ -12,48 +12,29 @@ import android.view.MotionEvent;
  * @Description：描述信息
  */
 public class NoScrollViewPager extends ViewPager {
-    private boolean noScroll = false;
+
+    // 是否禁止 viewpager 左右滑动
+    private boolean noScroll = true;
 
     public NoScrollViewPager(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
-    public NoScrollViewPager(Context context) {
-        super(context);
-    }
-
-    public void setNoScroll(boolean noScroll) {
-        this.noScroll = noScroll;
-    }
-
-    @Override
-    public void scrollTo(int x, int y) {
-        super.scrollTo(x, y);
-    }
-
     @Override
     public boolean onTouchEvent(MotionEvent arg0) {
-        if (noScroll)
+        if (noScroll){
             return false;
-        else
+        }else{
             return super.onTouchEvent(arg0);
+        }
     }
 
     @Override
     public boolean onInterceptTouchEvent(MotionEvent arg0) {
-        if (noScroll)
+        if (noScroll){
             return false;
-        else
+        }else{
             return super.onInterceptTouchEvent(arg0);
-    }
-
-    @Override
-    public void setCurrentItem(int item, boolean smoothScroll) {
-        super.setCurrentItem(item, smoothScroll);
-    }
-
-    @Override
-    public void setCurrentItem(int item) {
-        super.setCurrentItem(item);
+        }
     }
 }
